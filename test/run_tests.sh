6717fda7853d7815cf64d03c0eb609d1653b83d0
uncovsky <vit.uncovsky@gmail.com>
2024-04-05 16:09:05 +0200

    test times

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,17 +1,37 @@
-
+start=`date +%s`
 for f in ./all_satisfiable/*; do
 	../build/fousaty $f;
-  python3 check_model.py "model_file.txt" $f 
+  # python3 check_model.py "model_file.txt" $f 
 done
+
+ends=`date +%s`
+
+for f in ./all_satisfiable_100/*; do
+	../build/fousaty $f
+done
+
+ends100=`date +%s`
 
 echo "------ UNSAT ------"
 
 for f in ./all_unsat/*; do
 	../build/fousaty $f
 done
+endu=`date +%s`
 
 echo "------ BIG FAT UNSAT ------"
 
-for f in ./big_fat_unsat/*; do
+for f in ./all_unsat_100/*; do
 	../build/fousaty $f
 done
+endu100=`date +%s`
+
+#for f in ./big_fat_unsat/*; do
+#	../build/fousaty $f
+#done
+
+
+echo SAT time was `expr $ends - $start` seconds.
+echo SAT 100 time was `expr $ends100 - $ends` seconds.
+echo UNSAT easy time was `expr $endu - $ends100` seconds.
+echo UNSAT 100 vars time was `expr $endu100 - $endu` seconds.
